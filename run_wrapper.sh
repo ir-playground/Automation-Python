dd2f0d122305eec583db2f45589950518724ac39
ir-paras-oli <paras.oli@invisirisk.com>
2025-03-31 16:31:09 +0545

    Update run_wrapper.sh

--- run_wrapper.sh
+++ run_wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting leak_token.sh execution..."
-bash scripts/leak_token.sh  # Call the actual script
+bash scripts/secret_leak.sh  # Call the actual script
 bash scripts/statuscode.sh
 bash scripts/push.sh  # Call the actual script
 bash scripts/mimetype.sh  # Call the actual script
