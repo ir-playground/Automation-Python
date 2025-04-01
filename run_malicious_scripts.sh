7f528be6399a1c20dd51961a0a6f3e85174bad01
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-01 15:46:06 +0545

    Update run_malicious_scripts.sh

--- run_malicious_scripts.sh
+++ run_malicious_scripts.sh
@@ -5,4 +5,5 @@ bash malicious/secret_leak.sh  # Call the actual script
 bash malicious/statuscode.sh # Call the actual script
 bash malicious/push.sh  # Call the actual script
 bash malicious/mimetype.sh  # Call the actual script
+bash malicious/contentlength.sh  # Call the actual script
 echo "Execution completed."
