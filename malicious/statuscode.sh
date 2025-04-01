a6ed72c754fbf7518de71846658acab3c6643440
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-01 12:00:23 +0545

    Update statuscode.sh

--- malicious/statuscode.sh
+++ malicious/statuscode.sh
@@ -3,7 +3,7 @@
 echo "Checking status code from GitHub API..."
 
 # This request should trigger a 400 Bad Request because 'q' is empty
-curl -k -i -X GET "https://api.github.com/search/repositories?q="
+curl -k -i -X GET "https://api.veribom.com/graphql?q="
 
 # This request should trigger a 401 Unauthorized if authentication is required but not provided
 # curl -k -i -X GET "https://api.github.com/user"
