39d6ae4af18e547c09c5cfa70f0eb180b0951ffa
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-01 12:45:08 +0545

    Update mimetype.sh

--- malicious/mimetype.sh
+++ malicious/mimetype.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Fetch headers, follow redirects, and extract the MIME type (Content-Type)
-mime_type=$(curl -s -L -I -k "https://static.wixstatic.com/media/40296a_8b6d2c31d07343be92b810fc61492ba5~mv2.png/v1/fill/w_191,h_42,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/InvisiRisk%20Logo%20-%20transparent%20background.png" | grep -i 'Content-Type' | awk '{print $2}')
+mime_type=$(curl -s -L -I -k "https://github.com/ir-paras-oli/simple-django-project/blob/master/index.html" | grep -i 'Content-Type' | awk '{print $2}')
 
 # Force MIME type to 'image/png' if it's not detected correctly
-if [[ "$mime_type" != "image/png" ]]; then
-    mime_type="image/png"
+if [[ "$mime_type" != "text/html" ]]; then
+    mime_type="text/html"
 fi
 
 echo "MIME Type: $mime_type"
