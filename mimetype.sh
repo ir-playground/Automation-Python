28be4b7712e187bf079341a919d40d86b70f1e8f
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-01 09:36:47 +0545

    Update mimetype.sh

--- mimetype.sh
+++ mimetype.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -X GET czn7ca1z1wg0000gd0m0gx4usfyyyyyyb.oast.pro\
-     -H "Content-Type: application/json; charset=utf-8"
+curl -H "Accept: text/html" -o google.html "https://www.google.com"
+
