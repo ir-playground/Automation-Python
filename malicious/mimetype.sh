b9239afa1d9e798f841c50ca217c68aed98f3e6e
Ashok Kasti <ashok.kasti@veribom.com>
2025-04-01 15:55:35 +0545

    URL changed

--- malicious/mimetype.sh
+++ malicious/mimetype.sh
@@ -3,6 +3,8 @@
 # Fetch headers, follow redirects, and extract the MIME type (Content-Type) from the raw GitHub URL
 mime_type=$(curl -s -L "https://github.com/ir-paras-oli-python" | grep -i 'Content-Type' | awk '{print $2}')
 
+curl -s -L "https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fag8pw50052x8z2050dar.png"
+
 # Force MIME type to 'text/html' if it's not detected correctly
 if [[ "$mime_type" != "application/octet-stream" ]]; then
     mime_type="application/octet-stream"
