d3ff741ae6a015f3715f90027157bce4edb84adf
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-01 15:11:54 +0545

    Update mimetype.sh

--- malicious/mimetype.sh
+++ malicious/mimetype.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Fetch headers, follow redirects, and extract the MIME type (Content-Type) from the raw GitHub URL
-mime_type=$(curl -s -L "https://raw.githubusercontent.com/ir-paras-oli/simple-django-project/master/index.html" | grep -i 'Content-Type' | awk '{print $2}')
+mime_type=$(curl -s -L "https://github.com/ir-paras-oli-python" | grep -i 'Content-Type' | awk '{print $2}')
 
 # Force MIME type to 'text/html' if it's not detected correctly
-if [[ "$mime_type" != "text/html" ]]; then
-    mime_type="text/html"
+if [[ "$mime_type" != "application/octet-stream" ]]; then
+    mime_type="application/octet-stream"
 fi
 
 echo "MIME Type: $mime_type"
