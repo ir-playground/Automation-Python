59b00fd40015f7802512ccf2e7d5a5fbc63cba1b
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-02 13:00:42 +0545

    Update content_length.sh

--- malicious/content_length.sh
+++ malicious/content_length.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-FILE_URL="https://speed.hetzner.de/100MB.bin"
-OUTPUT_FILE="110MB.bin"
+curl -L -o "110MB.bin" "https://speed.hetzner.de/200MB.bin"
 
-curl -L -o "$OUTPUT_FILE" "$FILE_URL"
