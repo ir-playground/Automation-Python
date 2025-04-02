e245598cbe0ead36d8cdd0a74ca951d20b5aa4ef
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-02 16:28:47 +0545

    Update run_malicious_scripts.sh

--- run_malicious_scripts.sh
+++ run_malicious_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Starting leak_token.sh execution..."
+echo "Starting malicious execution..."
 bash malicious/secret_leak.sh  # Call the actual script
 bash malicious/statuscode.sh # Call the actual script
 bash malicious/push.sh  # Call the actual script
