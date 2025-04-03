294cfe03f4cd4234db18fd4621c06bd2c89a70b5
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-03 12:48:26 +0545

    Update status_code.sh

--- malicious_scripts/status_code.sh
+++ malicious_scripts/status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This request should send 422 status code.
-curl -k -i -X GET "https://api.veribom.com/graphql/projects?q="
+curl -i -X GET "https://api.veribom.com/graphql/projects?q="
