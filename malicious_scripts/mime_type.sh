3e4159225c9ce07fba20124b0fc5a93d39d9f3db
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-03 13:08:46 +0545

    Update mime_type.sh

--- malicious_scripts/mime_type.sh
+++ malicious_scripts/mime_type.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #This script downloads an image from a given URL using curl
-mime_type=$(curl -s -L "https://github.com/ir-paras-oli-python" | grep -i 'Content-Type' | awk '{print $2}')
 curl -s -L "https://tinyurl.com/invisirisk"
-echo "MIME Type: $mime_type"
