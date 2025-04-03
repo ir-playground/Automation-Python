61f0c100cf7fe07bcef76899278a8ff24cd17697
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-03 12:24:12 +0545

    Update mimetype.sh

--- malicious/mimetype.sh
+++ malicious/mimetype.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Fetch headers, follow redirects, and extract the MIME type (Content-Type) from the raw GitHub URL
-mime_type=$(curl -s -L "https://github.com/ir-paras-oli-python" | grep -i 'Content-Type' | awk '{print $2}')
+#This script downloads an image from a given URL using curl
 
-curl -s -L "https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fag8pw50052x8z2050dar.png"
 
-# Force MIME type to 'text/html' if it's not detected correctly
-if [[ "$mime_type" != "image/webp" ]]; then
-    mime_type="image/webp"
-fi
+curl -s -L "https://tinyurl.com/invisirisk"
 
 echo "MIME Type: $mime_type"
