4df5968e33e9d331831f0d54419d34a136c1d69c
ir-paras-oli <paras.oli@invisirisk.com>
2025-04-03 12:18:11 +0545

    Update push.sh

--- malicious/push.sh
+++ malicious/push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#This Bash script automates the process of committing and pushing a new file inside a folder to a Git repository(Pushed_file)
 
 echo "Marking repository as safe..."
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
@@ -7,10 +8,12 @@ echo "Setting up Git user..."
 git config user.name "ir-paras-oli"
 git config user.email "paras.oli@invisirisk.com"
 
-echo "Making changes (creating a file)..."
-echo "Some new content" > new6-file.txt
-git add new6-file.txt
-git commit -m "Add a new file"
+echo "Creating a folder and adding a file..."
+mkdir -p new_folder
+echo "Some new content" > Pushed_file/new6-file.txt
+
+git add new_folder/new6-file.txt
+git commit -m "Add a new file inside new_folder"
 
 echo "Pushing changes to the STAGE branch..."
 git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/${{ github.repository }}.git
