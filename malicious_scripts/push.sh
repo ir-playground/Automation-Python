7bd25bd50a3cb156fa019a823a5e7e75f62c1e78
Pranesh-Shrestha <pranesh.shrestha@tekvortex.com>
2025-04-16 16:27:35 +0545

    Update push.sh

--- malicious_scripts/push.sh
+++ malicious_scripts/push.sh
@@ -5,8 +5,8 @@ echo "Marking repository as safe..."
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
 echo "Setting up Git user..."
-git config user.name "ir-paras-oli"
-git config user.email "paras.oli@invisirisk.com"
+git config user.name "Pranesh-Shrestha"
+git config user.email "pranesh.shrestha@tekvortex.com"
 
 echo "Creating a folder and adding a file..."
 mkdir -p new_folder
@@ -17,4 +17,4 @@ git commit -m "Add a new file inside new_folder"
 
 echo "Pushing changes to the STAGE branch..."
 git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/${{ github.repository }}.git
-git push origin STAGE
+git push origin baseline
