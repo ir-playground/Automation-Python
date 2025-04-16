4ec2f8f9e16dc11d2dd2fca3a67d0a19072626e7
Pranesh-Shrestha <pranesh.shrestha@tekvortex.com>
2025-04-16 16:45:10 +0545

    Update push.sh

--- malicious_scripts/push.sh
+++ malicious_scripts/push.sh
@@ -17,4 +17,4 @@ git commit -m "Add a new file inside new_folder"
 
 echo "Pushing changes to the STAGE branch..."
 git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/${{ github.repository }}.git
-git push origin baseline
+git push origin anomaly-combined
