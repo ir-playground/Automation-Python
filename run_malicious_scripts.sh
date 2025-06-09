cc1b52759b29a004b4f93cb7de2815ad2bdb77c2
ir-pranesh-shrestha <pranesh.shrestha@invisirisk.com>
2025-06-09 10:36:39 +0545

    Update run_malicious_scripts.sh

--- run_malicious_scripts.sh
+++ run_malicious_scripts.sh
@@ -3,5 +3,4 @@
 echo "Starting malicious execution..."
 bash malicious_scripts/secret_leak.sh  # Call the actual script
 bash malicious_scripts/push.sh  # Call the actual script
-bash malicious_scripts/content_length.sh  # Call the actual script
 echo "Execution completed."
